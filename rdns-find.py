d461e5570e4aabf83bf78bef1969c58c6d349b02
m00np1e <73180416+m00np1e@users.noreply.github.com>
2021-07-20 11:34:48 -0500

    Add files via upload

--- rdns-find.py
+++ rdns-find.py
@@ -6,9 +6,9 @@ import csv
 import argparse
 
 
-# Performs reverse DNS lookups given a file of cidr and/or regular ip addresses
+# Performs reverse DNS lookups given a file of cidr and/or regular IP addresses
 # Input file must be delimited by a new line
-# Output file contains IP address and reverse DNS name, one per line
+# Output file contains csv IP address and reverse DNS
 # Ken Mininger, July 2021
 # kmininger@us.ibm.com
 
@@ -16,7 +16,7 @@ import argparse
 # get arguments
 def check_args():
     parser = argparse.ArgumentParser(
-        description="Performs reverse DNS lookups given a file of cidr and/or regular ip addresses",
+        description="Performs reverse DNS lookups given a file of cidr and/or regular IP addresses",
         prog="./rdns-find.py", usage="%(prog)s "
                                      "[options]")
     parser.add_argument("-i", help="Input file", required=True)
@@ -38,7 +38,6 @@ def open_output_file(outfile):
 def rev_dns(infile, outfile):
     try:
         print("IP Address     RDNS")
-        print("IP Address     RDNS", file=open((outfile), "a"))
         with open(infile) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter='\n')
             for row in csv_reader:
@@ -48,12 +47,15 @@ def rev_dns(infile, outfile):
                     addresses = row
                 add_len = len(addresses)
                 x = 0
-                while x < add_len:
-                    name, alias, addresslist = socket.gethostbyaddr(addresses[x])
-                    new_addr = ""
-                    print((new_addr.join(addresslist)), "-", name, file=open((outfile), "a"))
-                    print((new_addr.join(addresslist)), "-", name)
-                    x += 1
+                with open(outfile, "a") as csvfile:
+                    cvswriter = csv.writer(csvfile)
+                    while x < add_len:
+                        name, alias, addresslist = socket.gethostbyaddr(addresses[x])
+                        new_addr = ""
+                        data = (addresslist, name)
+                        cvswriter.writerow(data)
+                        print((new_addr.join(addresslist)), "-", name)
+                        x += 1
     except IOError:
         print("Error: Cannot open input file for reading or input file not found")
 
